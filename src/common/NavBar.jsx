15e5d9445cd64e65afba92aa41c9b435a1f6b02c
Kyalo M <jmutuku95@gmail.com>
2018-06-27 20:03:01 +0300

    [FX] correct logic

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -10,22 +10,23 @@ class NavBar extends Component{
   }
 
   render() {
-
+    console.log(this.state)
     let rightNav = ''
     if (this.state.isAuthenticated) {
+      rightNav = (
+        <ul className="nav navbar-right navbar-nav">
+         <li><Link to="/signout"> Sign Out </Link></li>
+        </ul>
+     )  
+    } else {
       rightNav = (
         <ul className="nav navbar-right navbar-nav">
           <li><Link to="/signup"> Sign Up </Link></li>
           <li><Link to="/signin"> Sign In </Link></li>
         </ul>
       )
-    } else {
-       rightNav = (
-         <ul className="nav navbar-right navbar-nav">
-           <li><Link to="/signout"> Sign Out </Link></li>
-         </ul>
-       )
     }
+
     return(
       <nav className="navbar navbar-inverse row">
         <div className="container-fluid">
@@ -38,7 +39,7 @@ class NavBar extends Component{
             <li><Link to="/orders"> Orders </Link></li>
             <li><Link to="/meals"> Meals</Link></li>
           </ul>
-          { rightNav}
+          { rightNav }
         </div>
       </nav>
 
