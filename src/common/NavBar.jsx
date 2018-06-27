e306e55657632377baa7d88f98b48a786a6cbd67
Kyalo M <jmutuku95@gmail.com>
2018-06-27 14:01:44 +0300

    Add the navigation bar component

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -1,7 +1,57 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component{
+  constructor(props){
+    super(props)
+    this.state = {isAuthenticated: this.props.isAuthenticated}
+    this.Signin = this.Signin.bind(this)
+    this.SignOut = this.SignOut.bind(this)
+  }
+
+  Signin(){
+    return(
+      <ul className="nav navbar-right navbar-nav">
+        <li><Link to="/signup"> Sign Up </Link></li>
+        <li><Link to="/signin"> Sign In </Link></li>
+      </ul>
+    );
+  }
+
+  SignOut(){
+    return(
+      <ul className="nav navbar-right navbar-nav">
+        <li><Link to="/signout"> Sign Out </Link></li>
+      </ul>
+    )
+  }
+
+  render(){
+    let rightNav;
+    if (this.state.isAuthenticated){
+      rightNav = <this.SignOut/>
+    } else {
+      rightNav = <this.Signin/>
+    }
+    return(
+      <nav className="navbar navbar-inverse row">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Hot Corner Delicacies</Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li className="active"><Link to="/"> Home </Link></li>
+            <li><Link to="/menu"> Menu </Link></li>
+            <li><Link to="/orders"> Orders </Link></li>
+            <li><Link to="/meals"> Meals</Link></li>
+          </ul>
+          { rightNav}
+        </div>
+      </nav>
+
+    );
+  }
 
 }
 
-export default NavBar
+export default NavBar;
