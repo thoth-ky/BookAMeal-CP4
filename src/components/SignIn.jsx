9232e5cab2afa08fb53644814f4641422460a11d
Kyalo M <jmutuku95@gmail.com>
2018-06-28 07:53:39 +0300

    Add signin component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { postData } from "../common/requests"
+
+class SignIn extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    var url = 'http://0.0.0.0:5000/api/v2/signin'
+    var data = { 'username': this.state.username, 'password': this.state.password }
+    postData(url, data)
+    console.log('res',)
+  }
+  render(){
+    return(
+      <div>
+        <form className="form-Group " onSubmit={this.handleSubmit}>
+                <h3>Sign In</h3>
+                <label>
+                    Username: <input type="text" className="form-control" name="username" onChange={this.handleChange} required />
+                </label>
+                <br/>
+                <label>
+                    Password: <input type="password" className="form-control" name="password" onChange={this.handleChange} required />
+                </label>
+                <br/>
+                <input className="btn btn-primary" type="submit" value="Sign In" />
+                <br/>
+                <p>Don't have an account?<Link to="/signup">Sign Up</Link> </p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
