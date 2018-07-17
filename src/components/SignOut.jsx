ae1fe7ff1263089c842d9961a86868e31db8dd77
Kyalo M <jmutuku95@gmail.com>
2018-07-17 10:07:14 +0300

    Add signout component

---
+++ src/components/SignOut.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class SignOut extends Component{
+  signOut = () => {
+    const url = '/api/v2/signout';
+    const access_token = localStorage.getItem('access_token');
+    fetch (url, {
+      headers: {
+        'Authorization': access_token,
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': "*",
+      },
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then((response)  => response.json())
+    .catch(error => console.error('Error: ', error))
+    .then(response => {
+      console.log('Success:', response.message)
+    })
+    sessionStorage.removeItem('access_token')
+    window.location.replace('/home')
+  }
+  render(){
+    this.signOut()
+    return(
+      <div>
+        <p>You have successfuly logged out</p>
+      </div>
+    )
+  }
+}
+
+export default SignOut;
