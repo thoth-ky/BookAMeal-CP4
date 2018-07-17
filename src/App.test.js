dfb170aed69653163f753d35a1db6d747b7a5b54
Kyalo M <jmutuku95@gmail.com>
2018-07-17 11:05:01 +0300

    Add test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import sessionStorage from 'mock-local-storage';
 import App from './App';
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('div').length).toBe(1)
 });
