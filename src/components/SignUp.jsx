b3e75810280d1881cb2474b7eef49d19a0fbc2ae
Kyalo M <jmutuku95@gmail.com>
2018-07-24 13:28:18 +0300

    Set is_admin after signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -31,6 +31,7 @@ class SignUp extends Component{
       .then(response => {
         if (response.access_token) {
             sessionStorage.setItem('access_token', response.access_token);
+            sessionStorage.setItem('is_admin', response.is_admin);
             console.log('Success', response.message);
         } else {
           alert('An error occured')
