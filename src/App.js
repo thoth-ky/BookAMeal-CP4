33ba76136fe3d6b422ccef5725a2bd444e3683d5
Kyalo M <jmutuku95@gmail.com>
2018-07-25 14:36:42 +0300

    Add redirects for protected routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Redirect } from "react-router-dom";
+import decode from "jwt-decode";
 import NavBar from "./common/NavBar";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import SignOut from "./components/SignOut";
+import Meals from "./components/Meals";
+import Meal from "./components/Meal";
+
+
+let isAdmin = '';
+const checkIsAuthenticated = () => {
+  const access_token = sessionStorage.getItem('access_token');
+  try {
+    var { exp, admin } = decode(access_token);
+    isAdmin = admin
+    if (exp > new Date().getTime()) {
+      return false
+    } else {
+      return true
+    }
+  } catch (e) {
+    return false
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    checkIsAuthenticated()? <Component {...props} />
+  : <Redirect to={{ pathname: '/signin', state: { from: props.location, redirectToReferrer: true } }} />
+  )} />
+
+)
+
 
 class App extends Component {
-
   render() {
-    const access_token = sessionStorage.getItem('access_token');
-    let authentication;
-    if (access_token){
-      authentication = true
-    }else {
-      authentication = false
-    }
+    let authentication = checkIsAuthenticated()
+
     return (
       <Router>
         <div className="container">
           <header>
-            <NavBar isAuthenticated={ authentication }/>
+            <NavBar isAuthenticated={ authentication } isAdmin={ isAdmin}/>
           </header>
           <Switch>
             <Route path="/signup" component={ SignUp }/>
             <Route path="/signin" component={ SignIn }/>
             <Route path="/signout" component={ SignOut }/>
+            <PrivateRoute exact path="/meals" component={ Meals }/>
+            <PrivateRoute path="/meals/:meal_id" component={ Meal } />
           </Switch>
         </div>
 
