91865281005ec2bda9348c33272244596683f6f2
Kyalo M <jmutuku95@gmail.com>
2018-07-25 14:35:42 +0300

    Declare Admin NavBar

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class NavBar extends Component{
   constructor(props){
     super(props)
     this.state = {
-      isAuthenticated: this.props.isAuthenticated
+      isAuthenticated: this.props.isAuthenticated,
+      isAdmin: this.props.isAdmin
     }
   }
 
@@ -14,30 +15,35 @@ class NavBar extends Component{
     if (this.state.isAuthenticated) {
       rightNav = (
         <ul className="nav navbar-right navbar-nav">
-         <li><Link to="/signout"> Sign Out </Link></li>
+         <li><NavLink to="/signout"> Sign Out </NavLink></li>
         </ul>
       )
 
       } else {
       rightNav = (
         <ul className="nav navbar-right navbar-nav">
-          <li><Link to="/signup"> Sign Up </Link></li>
-          <li><Link to="/signin"> Sign In </Link></li>
+          <li><NavLink to="/signup"> Sign Up </NavLink></li>
+          <li><NavLink to="/signin"> Sign In </NavLink></li>
         </ul>
       )
     }
-
+    let adminNav = '';
+    if (this.state.isAdmin) {
+      adminNav = (
+        <li><NavLink to="/meals"> Meals</NavLink></li>
+      )
+    }
     return(
       <nav className="navbar navbar-inverse row">
         <div className="container-fluid">
           <div className="navbar-header">
-            <Link className="navbar-brand" to="/">Hot Corner Delicacies</Link>
+            <NavLink className="navbar-brand" to="/">Hot Corner Delicacies</NavLink>
           </div>
           <ul className="nav navbar-nav">
-            <li className="active"><Link to="/"> Home </Link></li>
-            <li><Link to="/menu"> Menu </Link></li>
-            <li><Link to="/orders"> Orders </Link></li>
-            <li><Link to="/meals"> Meals</Link></li>
+            <li><NavLink to="/"> Home </NavLink></li>
+            <li><NavLink to="/menu"> Menu </NavLink></li>
+            <li><NavLink to="/orders"> Orders </NavLink></li>
+            { adminNav }
           </ul>
           { rightNav }
         </div>
