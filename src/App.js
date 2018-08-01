ccaa81a49527f68e8940d422c7b4828ea63677a1
Joseph Mutuku Kyalo <jmutuku95@gmail.com>
2018-08-01 10:09:53 +0300

    Revert merge into master "[FT} Add Menu component" (#7)
    * Revert "Add toggle nav for small screesn"
    
    This reverts commit c2e23c3118d4f0aab900fcf7d6116deeb55db38e.
    
    * Revert "Set is_admin after signup"
    
    This reverts commit 83728448e04f7bee6d8099ed87e37d3861ada4fa.
    
    * Revert "Set is_admin after login"
    
    This reverts commit 3294980354af187a5204abdab9d7b6b3d57d11b0.
    
    * Revert "Update Menu component"
    
    This reverts commit 6393a887e120307e0ffa0edb918dd5e2022f903f.
    
    * Revert "Update Menu Component"
    
    This reverts commit 94f380571dd1b1a9e306a779fcf34aa6d3a6be8a.
    
    * Revert "Add menu component to App"
    
    This reverts commit 64cf32c9f7d0a3f280b44a61e3cda6cd27d45a32.
    
    * Revert "Add Menu component"
    
    This reverts commit 118cc0bd69c05433cbdabd6f2fdae29125200ce5.
    
    * Revert "Add NavBar.jsx"
    
    This reverts commit 33a2a8744a6a8bb42f3631113423a57a0fa6e268.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./common/NavBar";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import SignOut from "./components/SignOut";
-import Menu from "./components/Menu"
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
   render() {
-    const access_token = sessionStorage.getItem('access_token');
-    const is_admin = sessionStorage.getItem('is_admin');
-    let authentication;
-    if (access_token){
-      authentication = true
-    }else {
-      authentication = false
-    }
     return (
-      <Router>
-        <div className="container">
-          <header>
-            <NavBar isAuthenticated={ authentication } isAdmin={ is_admin }/>
-          </header>
-          <Switch>
-            <Route path="/signup" component={ SignUp }/>
-            <Route path="/signin" component={ SignIn }/>
-            <Route path="/signout" component={ SignOut }/>
-            <Route path="/menu" component={ Menu }/>
-          </Switch>
-        </div>
-
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
