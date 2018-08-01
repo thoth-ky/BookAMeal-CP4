fafd72880ea734f6eb1c6d8f556b4824ea52be2a
Joseph Mutuku Kyalo <jmutuku95@gmail.com>
2018-08-01 10:08:31 +0300

    Revert "Add NavBar.jsx"
    This reverts commit 33a2a8744a6a8bb42f3631113423a57a0fa6e268.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./common/NavBar";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import SignOut from "./components/SignOut";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
   render() {
-    const access_token = sessionStorage.getItem('access_token');
-    let authentication;
-    if (access_token){
-      authentication = true
-    }else {
-      authentication = false
-    }
     return (
-      <Router>
-        <div className="container">
-          <header>
-            <NavBar isAuthenticated={ authentication }/>
-          </header>
-          <Switch>
-            <Route path="/signup" component={ SignUp }/>
-            <Route path="/signin" component={ SignIn }/>
-            <Route path="/signout" component={ SignOut }/>
-          </Switch>
-        </div>
-
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
