9255be43fb8ddb3e52e7e5b1b0c74763b0bceff7
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-08-16 17:37:53 +0300

    Implement Cart functionality

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Alert, Well, Modal } from "react-bootstrap";
+import { Button, Alert, Well, Modal, Table } from "react-bootstrap";
 import { timeConverter } from "../common/helpers";
 
 
@@ -12,10 +12,17 @@ class Menu extends Component{
       show_cart: false,
       cart: [],
       alert: null,
-      show_alert: false
+      show_alert: false,
+      quantity:null,
     }
     this.getMenu = this.getMenu.bind(this)
     this.getMenu()
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+    this.setState({ [event.target.name]: event.target.value })
+
   }
 
   showAlert = () => {
@@ -32,11 +39,28 @@ class Menu extends Component{
     this.setState({ alert: null})
   }
 
-  addToCart = (e) =>{
+  addToCart = (e) => {
+    // Add meal to the cart
     e.preventDefault()
-    alert('Adding to Cart')
+    let meal_id = e.currentTarget.dataset.id;
+    let meal_name = e.currentTarget.dataset.name;
+    let quantity = e.currentTarget.dataset.quantity;
+    let price = e.currentTarget.dataset.price;
+    let order = {
+      meal_id: meal_id,
+      meal_name: meal_name,
+      quantity:quantity,
+      price:price,
+      'sub': price*quantity
+    }
+    let current_orders = this.state.cart
+    current_orders = current_orders.concat(order)
+    this.setState({ cart: current_orders})
   }
 
+ removeFromCart = (e) => {
+   alert('remove from cart')
+ }
 
   Meal = (meal) => {
     console.log('Meal',meal.meal);
@@ -53,8 +77,8 @@ class Menu extends Component{
         <div className="col-md-2">Kes { price }.00</div>
         <div className="col-md-4">
           <form>
-              <label>Quantity: <input/></label>
-              <Button onClick={ this.addToCart } data-meal={ meal_item }>Add to Cart</Button>
+              <label>Quantity: <input type="number" min="0" name="quantity" onChange={ this.handleChange }/></label>
+              <Button onClick={ this.addToCart } data-id={ meal_id } data-quantity={ this.state.quantity } data-name={ name } data-price={ price }>Add to Cart</Button>
           </form>
         </div>
       </Well>
@@ -126,11 +150,48 @@ class Menu extends Component{
 
   render = () => {
     let menu = this.state.meals
+    let cart = this.state.cart
     const cartItems = () => {
+      const Row = (order) =>{
+
+        return (
+          <tr>
+            <td>{ order.order.meal_id }</td>
+            <td>{ order.order.meal_name }</td>
+            <td>{ order.order.quantity }</td>
+            <td>{ order.order.price }</td>
+            <td>{ order.order.sub }</td>
+            <td><Button onClick={ this.removeFromCart } data-id={ order.order.meal_id }>Remove</Button></td>
+          </tr>
+
+        )
+      }
+      const orderNode = cart.map((order) => {
+        return (< Row order={ order }  />)
+      })
       return(
-        <div>
-          <p>Cart Items Here</p>
-        </div>
+        <Well>
+          <Table>
+            <thead>
+              <tr>
+                <th>Meal ID</th>
+                <th>Meal Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Subtotal</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              { orderNode }
+            </tbody>
+            <tfooter>
+              <td colSpan="4">Total</td>
+              <td>Total</td>
+            </tfooter>
+          </Table>
+      </Well>
+
       )
     }
     return (
