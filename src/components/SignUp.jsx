f12be4b99233e6690c26f9e6eed3c6a46d74d8af
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-08-23 17:45:53 +0300

    Lint SignUp component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 
-class SignUp extends Component{
-  constructor(props){
+class SignUp extends Component {
+  constructor(props) {
     super(props)
     this.state = {
-      username: '' ,
+      username: '',
       email: '',
       password: '',
       password1: '',
-      alert: null
+      alert: null,
     }
   }
 
@@ -18,79 +18,86 @@ class SignUp extends Component{
     this.setState({ [event.target.name]: event.target.value })
   }
 
-  handleSubmit(event){
+  handleSubmit = (event) => {
     event.preventDefault();
-    if (this.state.password === this.state.password1 && this.state.password.length >= 8){
-      var url = '/api/v2/signup'
-      var data = { 'username': this.state.username, 'email': this.state.email, 'password': this.state.password }
+    const { password, password1, username, email } = this.state
+    if (password === password1 && password.length >= 8) {
+      const url = '/api/v2/signup'
+      const data = { username: username, email: email, password: password }
       fetch(url, {
         body: JSON.stringify(data),
         headers: {
           'content-type': 'application/json',
-          'Access-Control-Allow-Origin': "*",
+          'Access-Control-Allow-Origin': '*',
         },
         method: 'POST',
         mode: 'cors',
       })
-      .then((response)  => response.json())
-      .catch(error => console.error('Error: ', error))
-      .then(response => {
-        if (response.access_token) {
-            sessionStorage.setItem('access_token', 'Bearer ' + response.access_token);
-            console.log('Success', response.message);
+        .then(response => response.json())
+        .catch(error => console.error('Error: ', error))
+        .then((response) => {
+          if (response.access_token) {
+            sessionStorage.setItem('access_token', `Bearer ${response.access_token}`);
+            const msg = response.message
+            this.setState({ alert: msg })
+            console.log('Success: ', msg);
+            console.log('Token: ', response.access_token);
             window.location.replace('/')
-        } else {
-          this.setState({alert: response.message})
-          console.log(response.message)
-
-        }
-
-      })
+          } else {
+            const msg = response.message
+            this.setState({ alert: msg })
+          }
+        })
     } else {
-      let msg = 'Ensure passwords match and use more than 8 characters'
-      this.setState({alert: msg})
+      const msg = 'Ensure passwords match and use more than 8 characters'
+      this.setState({ alert: msg })
     }
   }
 
-  render(){
+  render() {
     let displayAlert = '';
-    if (this.state.alert){
+    const { alert } = this.state
+    if (alert) {
       displayAlert = (
         <div className="error">
-          <p>{ this.state.alert }</p>
+          <p>{ alert }</p>
         </div>
       )
     }
-    return(
+    return (
       <div>
 
-        <form className="form-Group w3-display-middle center" onSubmit={ this.handleSubmit }>
+        <form className="form-Group w3-display-middle center" onSubmit={this.handleSubmit}>
 
           <h3> Registration Form</h3>
           { displayAlert }
-          <label>
-            <span>Username:</span><input type="text" className="form-control" id="username"
-              placeholder="username" name="username" pattern=".{4,}"
-              onChange={this.handleChange} required title="Required minimum length of 4 characters"/>
+          <label htmlFor="username">
+            <span>Username:</span>
+            <input type="text" className="form-control" id="username" placeholder="username" name="username" pattern=".{4,}" onChange={this.handleChange} required title="Required minimum length of 4 characters" />
           </label>
-          <br/>
-          <label>
-              <span>Email:</span> <input type="email" className="form-control"
-                placeholder="example@mail.com" name="email" onChange={this.handleChange} required />
+          <br />
+          <label htmlFor="email">
+            <span>Email:</span>
+            <input type="email" className="form-control" placeholder="example@mail.com" name="email" onChange={this.handleChange} required />
           </label>
-          <br/>
-          <label>
-              <span>Password:</span> <input type="password" className="form-control" onChange={this.handleChange} name="password" pattern=".{8,}" required title="Atleast 8 characters." />
+          <br />
+          <label htmlFor="password">
+            <span>Password:</span>
+            <input type="password" className="form-control" onChange={this.handleChange} name="password" pattern=".{8,}" required title="Atleast 8 characters." />
           </label>
-          <br/>
-          <label>
-              <span>Confirm Password:</span> <input type="password" className="form-control" onChange={this.handleChange} name="password1" pattern=".{8,}" required title="Atleast 8 characters." />
+          <br />
+          <label htmlFor="password1">
+            <span>Confirm Password:</span>
+            <input type="password" className="form-control" onChange={this.handleChange} name="password1" pattern=".{8,}" required title="Atleast 8 characters." />
           </label>
-          <br/>
+          <br />
           <input className="btn btn-primary" type="submit" value="Sign Up" />
-            <br/>
-            <p>Already have an account?<a to="/signin" className="links">Sign In</a> </p>
-      </form>
+          <br />
+          <p>
+            {'Already have an account?' }
+            <a to="/signin" className="links">Sign In</a>
+          </p>
+        </form>
       </div>
     );
   }
