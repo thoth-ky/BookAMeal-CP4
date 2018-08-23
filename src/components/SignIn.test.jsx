0374819997a43101a29f43270b63da16fdbbf2d3
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-08-23 18:20:05 +0300

    Rename Sign in test to jsx

--- src/components/SignIn.test.jsx
+++ src/components/SignIn.test.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SignIn from'./SignIn.jsx';
+import SignIn from './SignIn';
 
 describe('These are tests for Signin component', () => {
   it('test that signin works', () => {
-    const wrapper = shallow(< SignIn />);
+    const wrapper = shallow(<SignIn />);
     const title = <h3>Sign In</h3>
-    const username = <span>Username:</span>
     expect(wrapper.find('div').length).toBe(1);
     expect(wrapper.find('form').length).toBe(1);
     expect(wrapper.find('label').length).toBe(2);
     expect(wrapper.contains(title)).toEqual(true);
   })
+
 })
