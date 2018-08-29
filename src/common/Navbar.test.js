851bf5ab313c914b451d3d21aff0b0f637364812
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-08-29 13:42:59 +0300

    remove unused imports

--- src/common/Navbar.test.js
+++ src/common/Navbar.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Link } from "react-router-dom";
-import Navbar from './Navbar';
+import { shallow} from 'enzyme';
+import Navbar from './NavBar';
 
 
-describe('These are navbar tests', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(< Navbar />);
+describe ('These are navbar tests', () => {
+  it ('renders navbar without crashing', () => {
+    const wrapper = shallow(<Navbar />);
     const hcd = 'Hot Corner Delicacies'
     expect(wrapper.find('nav').length).toBe(1);
     expect(wrapper.find('div').length).toBe(2);
