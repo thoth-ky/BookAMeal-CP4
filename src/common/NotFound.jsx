4c5e857393eda4d83b368a1726d0f0b1caa950de
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-09-04 12:40:42 +0300

    Add not found page

---
+++ src/common/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Well } from 'react-bootstrap';
+
+
+const NotFound = () => (
+  <Well className="center">
+    <h2>HTTP 404</h2>
+    <h3>Sorry, this page was not found.</h3>
+    <div>
+      <p>Go back<Link className="links" to="/">Home</Link></p>
+    </div>
+  </Well>
+);
+
+export default NotFound;
