696669345cfa914692bda0fddd953b9323e00771
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-09-06 09:50:24 +0300

    [CH] Remove unneccessary files

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
-import raf from './tempPolyfills'
+/*
+As of Enzyme 3, Enzyme requires an Adapter corresponding to the version of React used.
+(This project uses examples the adapter for React 16.)
 
+The adapter is configured configured here.
+*/
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
